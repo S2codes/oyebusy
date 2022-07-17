39a66b5959abab533efb295ba1bca2af74ecb266
S2codes <25subhankar@gmail.com>
2022-07-17 13:16:48 +0530

    desktop mode complete

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    console.log('hello querry');
+    
+    // header slider 
+    $('.offers').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        prevArrow:"<i class='bi bi-arrow-right-circle offer-arrow-pre'></i>",
+        nextArrow: "<i class='bi bi-arrow-left-circle offer-arrow-next'></i>"
+    })
+
+
+    $('#trnd-service-1').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        prevArrow:"<i class='bi bi-arrow-left-circle trend-arrow-pre'></i>",
+        nextArrow: "<i class='bi bi-arrow-right-circle trend-arrow-next'></i>"
+    })
+    $('#trnd-service-2').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        prevArrow:"<i class='bi bi-arrow-left-circle trend-arrow-pre'></i>",
+        nextArrow: "<i class='bi bi-arrow-right-circle trend-arrow-next'></i>"
+    })
+    $('#trnd-service-3').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        prevArrow:"<i class='bi bi-arrow-left-circle trend-arrow-pre'></i>",
+        nextArrow: "<i class='bi bi-arrow-right-circle trend-arrow-next'></i>"
+    })
+
+
+
+})
